198acef0eb71bed88a646e4de425f22e7c46273e
Kim Avillanosa <avillanosakzxc@gmail.com>
2023-08-31 11:46:15 +0800

    added initial files

---
+++ app.browser/app.browser/Browser.cs
@@ -0,0 +1,75 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace app.browser
+{
+    public class Browser : IDisposable
+    {
+        private readonly BrowserConfiguration browserConfiguration;
+        public IWebDriver Driver { get; set; }
+
+        public Browser(BrowserConfiguration browserConfiguration)
+        {
+            this.browserConfiguration = browserConfiguration;
+            Initialize();
+        }
+
+        public async Task Wait(By xpath)
+        {
+            if (Driver == null)
+                throw new Exception("Browser not initialized");
+
+            WebDriverWait wait = new WebDriverWait(Driver, browserConfiguration.TimeOut);
+            wait.Until(ExpectedConditions.ElementExists(xpath));
+        }
+
+
+        /// <summary>
+        /// for remote access https://novnc.com/info.html
+        /// default novnc : secret
+        /// http://localhost:7900/?autoconnect=1&resize=scale&password=secret
+        /// 
+        /// Docker Referrences: https://hub.docker.com/r/selenium/standalone-chrome
+        /// </summary>
+        /// <exception cref="ApplicationException"></exception>
+        public void Initialize()
+        {
+            var remoteUrl = new Uri($"http://127.0.0.1:4444/wd/hub");
+            var webDriverManager = new WebDriverManager.DriverManager();
+
+            webDriverManager.SetUpDriver(new ChromeConfig());
+
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddExcludedArgument("enable-automation");
+
+            if (browserConfiguration.IsHeadless)
+                chromeOptions.AddArgument("--headless");
+
+            Driver = browserConfiguration.IsRemote ?
+            new RemoteWebDriver(remoteUrl, chromeOptions) : new ChromeDriver(chromeOptions);
+
+            if (browserConfiguration.IsMax)
+                Driver.Manage().Window.Maximize();
+
+            Driver.Navigate().GoToUrl(browserConfiguration.BaseUrl);
+
+            var wait = new WebDriverWait(Driver, browserConfiguration.TimeOut);
+
+            var loaded = wait.Until(d => { return d.Url.Contains(browserConfiguration.BaseUrl); });
+
+            if (!loaded)
+                throw new ApplicationException($"{browserConfiguration.BaseUrl} was not loaded");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Driver has been disposed");
+            Driver.Dispose();
+        }
+    }
+}
---
+++ app.browser/app.browser/BrowserConfiguration.cs
@@ -0,0 +1,43 @@
+﻿namespace app.browser
+{
+    public class BrowserConfiguration
+    {
+
+        private readonly bool isRemote;
+        private readonly bool isMax;
+        private readonly string baseUrl;
+        private readonly int timeout;
+        private readonly bool isHeadless;
+
+        /// <summary>
+        /// if true use web driver
+        /// </summary>
+        public bool IsRemote => isRemote;
+        /// <summary>
+        /// if true maximize browser, only if it is not remote driver
+        /// </summary>
+        public bool IsMax => isMax && !isRemote;
+        /// <summary>
+        /// run headless
+        /// </summary>
+        public bool IsHeadless => isHeadless;
+        /// <summary>
+        /// base url of the solution
+        /// </summary>
+        public string BaseUrl => baseUrl;
+        /// <summary>
+        /// scraping timeout
+        /// </summary>
+        public TimeSpan TimeOut => TimeSpan.FromSeconds(timeout);
+
+        public BrowserConfiguration(bool isMax, string baseUrl,
+            int timeout, bool isHeadless, bool isRemote)
+        {
+            this.isRemote = isRemote;
+            this.isMax = isMax;
+            this.baseUrl = baseUrl;
+            this.timeout = timeout;
+            this.isHeadless = isHeadless;
+        }
+    }
+}
